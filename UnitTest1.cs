5718b75f9859061d04adbf42d65a9ef2b139542f
Admin <Admin@DESKTOP-K4H2QFB>
2024-01-25 19:35:13 +0700

    Добавьте файлы проекта.

---
+++ UnitTest1.cs
@@ -0,0 +1,48 @@
+using NuGet.Frameworks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace TestGosuslugi
+{
+    [TestFixture]
+    public class ChangePasswordTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Создаем экземпляр драйвера
+            driver = new EdgeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        }
+        
+        public IWebElement WaitFindElement(By by, bool clicked = false)
+        {
+
+            IWebElement element = wait.Until(driver => driver.FindElement(by));
+            if (clicked) element.Click();
+            return element;
+        } 
+
+
+        [Test]
+        public void OpenChangePasswordFormTest()
+        {
+            driver.Navigate().GoToUrl("https://www.gosuslugi.ru");
+            WaitFindElement(By.ClassName("login-button"), true);
+            WaitFindElement(By.XPath("//button[text()=\" Не удаётся войти? \"]"), true);
+            WaitFindElement(By.XPath("//button[text()=\" восстановления пароля \"]"), true); 
+            Assert.IsTrue(WaitFindElement(By.TagName("esia-recovery")).Displayed);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
